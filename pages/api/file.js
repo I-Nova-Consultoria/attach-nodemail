c3b6fccf7d7f280a6c07158bcf014e63ee15f478
mahanmashoof <mahan.mashoof@gmail.com>
2021-03-16 10:26:23 -0300

    send email + info ok

--- pages/api/file.js
+++ pages/api/file.js
@@ -15,6 +15,7 @@ export const config = {
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
+
     const form = new formidable.IncomingForm();
     form.uploadDir = `./temp`;
     form.keepExtensions = true;
@@ -22,76 +23,78 @@ export default function handler(req, res) {
       //rename the incoming file to the file's name
       file.path = form.uploadDir + "/" + file.name;
     })
+
     const formContent = form.parse(req, (err, fields, files) => {
-      console.log(fields);
-    });
 
-    setTimeout(() => {
-      const fileName = formContent.openedFiles[0].name;
-      var pathToAttachment = formContent.openedFiles[0].path;
-      var attachment = fs.readFileSync(path.resolve(pathToAttachment)).toString("base64");
+      setTimeout(() => {
+        const fileName = formContent.openedFiles[0].name;
+        var pathToAttachment = formContent.openedFiles[0].path;
+        var attachment = fs.readFileSync(path.resolve(pathToAttachment)).toString("base64");
 
-      /*var transport = {
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: {
-          user: 'i.novaimobiliaria3@gmail.com',
-          pass: '1n0v4@2021'
-        }
-      };
+        var content = `Prezado Colaborador de RH, você acaba de receber um novo perfil de I.novador! Seguem os dados:
+        \n ${fields.corName} \n ${fields.corCel} \n ${fields.corEmail} \n ${fields.corMessage}`;
 
-      var transporter = nodemailer.createTransport(transport);
+        var mail = {
+          from: "aprovafacil.inventos@aprovafacil.net",
+          to: 'mahan.mashoof@gmail.com',
+          subject: 'Wohooo! Novo perfil de I.novador!',
+          text: content,
+          attachments: [
+            {
+              content: attachment,
+              filename: fileName,
+              type: "application/pdf",
+              disposition: "attachment"
+            }
+          ]
+        };
 
-      transporter.verify((error, success) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Server is ready to take messages');
-        }
-      });*/
-
-      var content = `Prezado Colaborador de RH, você acaba de receber um novo perfil de I.novador! Seguem os dados:
-      \n Nome inovador \n Tel inovador \n email inovador \n mensagem inovador`;
-
-      var mail = {
-        from: "aprovafacil.inventos@aprovafacil.net",
-        to: 'mahan.mashoof@gmail.com',
-        subject: 'Wohooo! Novo perfil de I.novador!',
-        text: content,
-        attachments: [
-          {
-            content: attachment,
-            filename: fileName,
-            type: "application/pdf",
-            disposition: "attachment"
+        sgMail.send(mail, (err, data) => {
+          if (err) {
+            res.json({
+              status: 'fail',
+              err
+            });
+          } else {
+            res.json({
+              status: 'success'
+            });
           }
-        ]
-      };
-
-      sgMail.send(mail, (err, data) => {
-        if (err) {
-          res.json({
-            status: 'fail',
-            err
-          });
-        } else {
-          res.json({
-            status: 'success'
-          });
-        }
-      });
-      /*transporter.sendMail(mail, (err, data) => {
-        if (err) {
-          res.json({
-            status: 'fail',
-            err
-          });
-        } else {
-          res.json({
-            status: 'success'
-          });
-        }
-      });*/
-    }, 1000);
+        });
+      }, 1000);
+    });
   }
 }
+
+
+//SMTP mail option:
+/*var transport = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: 'i.novaimobiliaria3@gmail.com',
+    pass: '1n0v4@2021'
+  }
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});*/
+/*transporter.sendMail(mail, (err, data) => {
+  if (err) {
+    res.json({
+      status: 'fail',
+      err
+    });
+  } else {
+    res.json({
+      status: 'success'
+    });
+  }
+});*/
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { post } from 'axios';
 
 function SimpleReactFileUpload() {
 
+  const [corName, setCorName] = useState('')
+  const [corCel, setCorCel] = useState('')
+  const [corEmail, setCorEmail] = useState('')
+  const [corMessage, setCorMessage] = useState('')
   const [file, setFile] = useState([])
 
-  function fileUpload(file){
+  function fileUpload() {
     const url = 'http://localhost:3000/api/file';
     const formData = new FormData();
-    formData.append('file',file)
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-    return post(url, formData,config)
+    formData.append('corFile', file)
+    formData.append('corName', corName)
+    formData.append('corCel', corCel)
+    formData.append('corEmail', corEmail)
+    formData.append('corMessage', corMessage)
+    //var options = { content: formData };
+    return post(url, formData)
   }
 
-  function onFormSubmit(e){
+  function onFormSubmit(e) {
     e.preventDefault()
-    fileUpload(file).then((response)=>{
+    fileUpload(file).then((response) => {
       console.log(response.data);
-    })
+    });
   }
 
   function onFileChange(e) {
@@ -29,12 +33,44 @@ function SimpleReactFileUpload() {
     setFile(file)
   }
 
-  return(
-    <form onSubmit={onFormSubmit}>
-        <h1>File Upload</h1>
-        <input type="file" onChange={onFileChange} />
-        <button type="submit">Submit</button>
-      </form>
+  function handleName(e) {
+    const value = e.target.value
+    setCorName(value);
+  }
+
+  function handleCel(e) {
+    const value = e.target.value
+    setCorCel(value);
+  }
+
+  function handleEmail(e) {
+    const value = e.target.value
+    setCorEmail(value);
+  }
+
+  function handleMessage(e) {
+    const value = e.target.value
+    setCorMessage(value);
+  }
+
+  return (
+    <form id='myForm' onSubmit={onFormSubmit}>
+      <h1>File Upload</h1>
+      <input name='corName' className='form-field main' type="text" placeholder='Nome' value={corName} onChange={handleName} required />
+      <div>
+        <input
+          name='corCel' type="text" className='form-field main' mask="(99) 99 99 99 999" placeholder='Celular' value={corCel} onChange={handleCel} required
+        />
+      </div>
+      <input
+				name='corEmail' type="email" className='form-field main' placeholder='E-mail' value={corEmail} onChange={handleEmail} required
+			/>
+      <input
+				name='corMessage' type="text" className='form-field text' InputProps={{ disableUnderline: true }} placeholder='Mensagem' multiline value={corMessage} onChange={handleMessage} required
+			/>
+      <input name='corFile' type="file" onChange={onFileChange} />
+      <button type="submit" >Submit</button>
+    </form>
   );
 }
 
